cc3ae89886076f3f58d2e031446053b2ed19a2c2
glicka <glicka@users.noreply.github.com>
2017-08-23 13:49:24 -0700

    New Changes

--- Reconstruction Codes/dataProcessing.py
+++ Reconstruction Codes/dataProcessing.py
@@ -11,7 +11,8 @@ def dataProcessing(datafile):
     import numpy as np
     import matplotlib.pyplot as plt
     import pylab
-    
+
+#### Extract data here ####    
     f = h5py.File(datafile, 'r')
     #print(f)
     #print("Keys: %s" % f.keys())
@@ -21,11 +22,37 @@ def dataProcessing(datafile):
     groupTimeVals = f.keys()[5]
     timeVals = list(f[groupTimeVals])
     rawData = list(f[groupKeyVal])
+    
+    rawDataMat = np.matrix(rawData)
+    temp = [i for i in groupDetectorVal if i <= 12]
+    temp1 = [i for i in groupDetectorVal if i > 12]
+    plane1Data = np.zeros((len(temp),len(rawDataMat[0,:])))
+    plane2Data = np.zeros((len(temp1),len(rawDataMat[0,:])))
+    plane1Times = np.zeros((len(temp),1))
+    plane2Times = np.zeros((len(temp2),1))
+
+#### Separate data based on each plane of detectors ####
+    n = 0
+    m = 0
+    for i in range(0,len(groupDetectorVal)):
+        if groupDetectorVal[i] <= 12:
+            plane1Data[n,:] = rawDataMat[i,:]
+            plane1Times[n] = timeVals[i]
+            n = n+1
+        else:
+            plane2Data[m,:] = rawDataMat[i,:]
+            plane2Times[m] = timeVals[i]
+            m = m+1
+            
     #A = RawSamples.rawData
     #print(rawData)
-    rawDataMat = np.matrix(rawData)
-    neutronPulseData = PSD(rawDataMat,'NE204-09-21-2011-Cs137-LS-Signal.csv')
-    cones = generateCones(detectorVals,timeVals,neutronPulseData)
+
+#### Perform PSD for each plane of data to extract just neutron information ####
+    neutronPulseData1 = PSD(plane1Data,'NE204-09-21-2011-Cs137-LS-Signal.csv')
+    neutronPulseData2 = PSD(plane2Data,'NE204-09-21-2011-Cs137-LS-Signal.csv')
+
+#### Generate cones and do energy reconstruction of neutrons ####
+    cones = generateCones(detectorVals,plane1Times,plane2Times,neutronPulseData1,neutronPulseData2)
     #for i in range(0,100):#range(0,len(rawDataMat[:,0])):
     #    print 'i = ', i
     #    fig = rawDataMat[i,:]
--- Reconstruction Codes/generateCones.py
+++ Reconstruction Codes/generateCones.py
@@ -6,7 +6,7 @@ Created on Wed Aug 16 10:31:57 2017
 @author: aglick
 """
 
-def generateCones(detectorVals,timeVals,neutronPulseData):
+def generateCones(detectorVals,plane1Times,plane2Times,neutronPulseData1,neutronPulseData2):
     import csv
     import numpy as np
     import math
@@ -15,29 +15,29 @@ def generateCones(detectorVals,timeVals,neutronPulseData):
     import pylab
     
     
-    dataListPlane2 = []
-    m=0
-    k=0
-    for i in range(0,len(neutronPulseData)-1):
-        if detectorVals[i] <= 11:
-            m = m + 1
+#    dataListPlane2 = []
+#    m=0
+#   k=0
+#   for i in range(0,len(neutronPulseData)-1):
+#        if detectorVals[i] <= 11:
+#            m = m + 1
             #dataListPlane1 [i,:] = np.insert([neutronPulseData[i],timeVals[i])
-        else:
-            k = k + 1
+#        else:
+#            k = k + 1
             
-    adcBit = len(neutronPulseData[0,:])        
-    dataListPlane1 = np.zeros([m,adcBit+2])
-    dataListPlane2 = np.zeros([k,adcBit+2])
+#    adcBit = len(neutronPulseData[0,:])        
+#    dataListPlane1 = np.zeros([m,adcBit+2])
+#    dataListPlane2 = np.zeros([k,adcBit+2])
     
-    for i in range(0,len(neutronPulseData)-1):
-        if detectorVals[i] <=11:
-            dataListPlane1[i,:] = [neutronPulseData[i,:],timeVals[i],detectorVals[i]]
-        else:
-            dataListPlane2[i,:] = [neutronPulseData[i,:],timeVals[i],detectorVals[i]]
+#    for i in range(0,len(neutronPulseData)-1):
+#        if detectorVals[i] <=11:
+#            dataListPlane1[i,:] = [neutronPulseData[i,:],timeVals[i],detectorVals[i]]
+#        else:
+#            dataListPlane2[i,:] = [neutronPulseData[i,:],timeVals[i],detectorVals[i]]
     
     
-    dataListPlane1 = dataListPlane1[np.argsort(dataListPlane1[:,adcBit+1])]
-    dataListPlane2 = dataListPlane2[np.argsort(dataListPlane1[:,adcBit+1])]
+#    dataListPlane1 = dataListPlane1[np.argsort(dataListPlane1[:,adcBit+1])]
+#    dataListPlane2 = dataListPlane2[np.argsort(dataListPlane1[:,adcBit+1])]
     
     
     hist = np.zeros(len(dataListPlane1[:,0]))
