3e238655322ce4da2e8e97f14229595498717013
Adam Glick <adam_glick@berkeley.edu>
2018-02-13 09:16:55 -0800

    added HV controller

---
+++ SIS-sis3320/linux_drivers/getVMEcontroller.sh
@@ -0,0 +1,10 @@
+cd
+mkdir WIENER
+cd WIENER
+wget https://iseg-hv.com/download/SOFTWARE/isegVMEcontrol/current/isegVmeControl-ubuntu64-1.0.8.7.zip
+unzip -xvf isegVmeControl-ubuntu64-1.0.8.7.zip
+cd isegVmeControl-ubuntu64-1.0.8.7
+if ! ldconfig -p | grep qt-sdk; then
+  sudo apt-get install qt-sdk
+fi
+sudo mv libxx_usb.so /usr/lib
