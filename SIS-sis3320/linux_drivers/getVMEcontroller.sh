bae91bf97edbee0c089071bdd21e2e9b2ffe9e9f
Adam Glick <adam_glick@berkeley.edu>
2018-02-13 11:40:07 -0800

    fixed iseg installer

--- SIS-sis3320/linux_drivers/getVMEcontroller.sh
+++ SIS-sis3320/linux_drivers/getVMEcontroller.sh
@@ -2,7 +2,7 @@ cd
 mkdir WIENER
 cd WIENER
 wget https://iseg-hv.com/download/SOFTWARE/isegVMEcontrol/current/isegVmeControl-ubuntu64-1.0.8.7.zip
-unzip -xvf isegVmeControl-ubuntu64-1.0.8.7.zip
+unzip isegVmeControl-ubuntu64-1.0.8.7.zip
 cd isegVmeControl-ubuntu64-1.0.8.7
 if ! ldconfig -p | grep qt-sdk; then
   sudo apt-get install qt-sdk
